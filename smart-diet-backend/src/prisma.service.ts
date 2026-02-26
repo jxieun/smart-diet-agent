73b6147d080081b13a50ca090d39bc9d733241be
JIEUN <163732805+jxieun@users.noreply.github.com>
2026-02-26 13:21:35 +0900

    npx prisma generate 오류 수정

--- smart-diet-backend/src/prisma.service.ts
+++ smart-diet-backend/src/prisma.service.ts
@@ -1,15 +1,13 @@
-// src/prisma.service.ts
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import 'dotenv/config';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   constructor() {
-    // 'as any'를 붙여서 타입스크립트의 엄격한 체크를 잠시 우회합니다.
-    // Prisma 7 실행 시에는 이 값이 정상적으로 전달됩니다.
-    super({
-      datasourceUrl: process.env.DATABASE_URL,
-    } as any);
+    const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+    super({ adapter });
   }
 
   async onModuleInit() {
