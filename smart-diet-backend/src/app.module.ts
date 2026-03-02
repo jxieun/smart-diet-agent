4253427f4998fe69ab2ad2608b6b1c695394a0a3
JIEUN <163732805+jxieun@users.noreply.github.com>
2026-03-02 15:58:42 +0900

    app.module.ts 수정

--- smart-diet-backend/src/app.module.ts
+++ smart-diet-backend/src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module'; // 1. UsersModule을 불러옴
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, AuthModule], // 2. 여기에 추가해야 서버가 UsersController와 AuthController를 인식함
+  imports: [UsersModule, 
+    // isGlobal: true를 설정하면 모든 모듈에서 별도의 import 없이 ConfigService를 쓸 수 있습니다.
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule], // 2. 여기에 추가해야 서버가 UsersController와 AuthController를 인식함
   controllers: [AppController],
   providers: [AppService],
 })
