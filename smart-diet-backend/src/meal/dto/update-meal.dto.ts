6303a99b1e5303164c63430fc03cabacd81f837e
JIEUN <163732805+jxieun@users.noreply.github.com>
2026-03-04 12:28:10 +0900

    update dto를 @nestjs/swagger추가

--- smart-diet-backend/src/meal/dto/update-meal.dto.ts
+++ smart-diet-backend/src/meal/dto/update-meal.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { CreateMealDto } from './create-meal.dto';
 
 // CreateMealDto의 모든 필드를 선택사항(Optional)으로 상속받음
