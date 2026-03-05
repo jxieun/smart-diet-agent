18df7c4385d754c8d20657dbc838d79c93789dfe
JIEUN <163732805+jxieun@users.noreply.github.com>
2026-03-05 10:34:30 +0900

    module 주석 추가

--- smart-diet-backend/src/reports/reports.module.ts
+++ smart-diet-backend/src/reports/reports.module.ts
@@ -1,3 +1,5 @@
+// src/reports/reports.module.ts
+
 import { Module } from '@nestjs/common';
 import { ReportsService } from './reports.service';
 import { ReportsController } from './reports.controller';
