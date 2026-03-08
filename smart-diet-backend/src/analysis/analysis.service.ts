089c34cd68f1f3151bd4ff85a267e514d2682ced
JIEUN <163732805+jxieun@users.noreply.github.com>
2026-03-08 12:28:07 +0900

    서비스에서 메시지 발행

--- smart-diet-backend/src/analysis/analysis.service.ts
+++ smart-diet-backend/src/analysis/analysis.service.ts
@@ -1,10 +1,11 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { AmqpConnection } from '@golevelup/nestjs-rabbitmq';
 import { PrismaService } from '../prisma/prisma.service';
 import { AnalysisStatus } from '@prisma/client';
 
 @Injectable()
 export class AnalysisService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService, private readonly amqpConnection: AmqpConnection,) {}
 
   async createAnalysisTask(userId: string, imageUrl: string) {
     // 1. 유저 정보를 조회하여 일일 목표 칼로리(goalCalories) 가져오기
@@ -27,15 +28,14 @@ export class AnalysisService {
       },
     });
 
-    // 3. Saga 패턴 시작: FastAPI로 보낼 메시지에 목표 칼로리 포함
-    // 이 정보가 나중에 AI 엔진이 조언을 생성하는 기준이 됩니다.
-    /*
-    this.amqpConnection.publish('menu_analysis_exchange', 'analyze', { 
-      taskId: task.id, 
-      imageUrl, 
-      targetCalories: user.goalCalories 
+    // 2. 🚀 RabbitMQ로 분석 요청 메시지 전송
+    // 'menu_analysis_exchange' 통로로 'analyze.request'라는 이름표를 붙여서 보냄
+    await this.amqpConnection.publish('menu_analysis_exchange', 'analyze.request', {
+      taskId: task.id,
+      imageUrl: task.imageUrl,
+      targetCalories: task.targetCalories,
+      userId: task.userId,
     });
-    */
 
     return task;
   }
